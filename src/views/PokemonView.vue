e7487328fb3f3f3edbe02d32ba0ff3fdbc5e77d0
nestor-nurod <nestor.nurod@gmail.com>
2023-08-29 20:24:27 -0600

    Pagination

--- src/views/PokemonView.vue
+++ src/views/PokemonView.vue
@@ -1,11 +1,9 @@
 <script setup>
-import axios from 'axios';
 import { useRoute, useRouter } from 'vue-router';
 import LoadingSpinner from '../components/LoadingSpinner.vue';
-import { ref } from 'vue';
+import {useGetData} from '@/composables/getData';
 
 
-const pokemonInfo = ref({})
 
 const route = useRoute()
 const router = useRouter();
@@ -14,38 +12,27 @@ const back = () => {
     router.push('/pokemons')
 }
 
-const loading = ref(true)
+const {data, getData, loading, error} = useGetData()
 
-const getPokemonData = async () => {
-    try {
-        const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${route.params.name}`);
-        pokemonInfo.value = data
-        loading.value = false
-    } catch (error) {
-        console.log(error)
-        pokemonInfo.value = null;
-        loading.value = false
-    }
-}
 
-getPokemonData()
+getData(`https://pokeapi.co/api/v2/pokemon/${route.params.name}`)
 
 </script>
 
 <template>
     <LoadingSpinner v-if="loading" class="mt-5"></LoadingSpinner>
-    <div v-else class="container">
+    <div class="container">
         <div class="row text-center">
-            <div v-if="pokemonInfo">
+            <div v-if="data">
                 <div class="col-md-12">
                     <h1>{{ $route.params.name }}</h1>
                 </div>
                 <div class="col-md-12">
-                    <img style="width: 300px; height: auto;" :src="pokemonInfo.sprites?.front_default"
-                        :alt="pokemonInfo.name">
+                    <img style="width: 300px; height: auto;" :src="data.sprites?.front_default"
+                        :alt="data.name">
                 </div>
             </div>
-            <div v-else class="col-md-12 mt-5">
+            <div v-if="error" class="col-md-12 mt-5">
                 <h1>El pokemon ingresado no existe...</h1>
                 <img src="@/assets/ditto.png" alt="ditto">
             </div>
--- src/views/PokemonsView.vue
+++ src/views/PokemonsView.vue
@@ -1,32 +1,37 @@
 <script setup>
-import axios from 'axios';
-import {ref} from 'vue';
 import {RouterLink} from 'vue-router';
+import {useGetData} from '@/composables/getData';
+import LoadingSpinner from '../components/LoadingSpinner.vue';
 
-import PokemonCard from '../components/PokemonCard.vue';
 
-const pokemons = ref([]);
+const {data, getData, loading, error} = useGetData();
 
-const getData = async () => {
-    try {
-        const {data} = await axios.get("https://pokeapi.co/api/v2/pokemon")
-        pokemons.value = data.results;
-    } catch (error) {
-        console.log(error)
-    }
-}
-getData()
+getData("https://pokeapi.co/api/v2/pokemon");
 
 </script>
 
 <template>
-    <div class="container">
-        <h1>Pokemons</h1>
+    <h1>Pokemons</h1>
+    <div v-if="error" class="alert alert-danger" role="alert">
+        Ha ocurrido un error al recuperar la información
+    </div>
+    <LoadingSpinner v-if="loading" class="mt-5"></LoadingSpinner>
+    <div v-if="data" class="container">
         <div class="row">
-            <ul>
-                <li :id="index" v-for="(pokemon, index) in pokemons" :key="index"><RouterLink :to="`/pokemons/${pokemon.name}`">{{ pokemon.name }}</RouterLink></li>
+            <ul class="list-group">
+                <li class="list-group-item mb-1" :id="index" v-for="(pokemon, index) in data.results" :key="index"><RouterLink class="custom-link" :to="`/pokemons/${pokemon.name}`">{{ pokemon.name }}</RouterLink></li>
             </ul>
             
         </div>
     </div>
+    <div class="mt-3 text-center mb-5">
+        <button :disabled="!data?.previous" @click="getData(data.previous)" class="btn btn-warning me-2">Prev</button>
+    <button :disabled="!data?.next" @click="getData(data.next)" class="btn btn-primary">Sig</button>
+    </div>
 </template>
+
+<style>
+.custom-link{
+    text-decoration: none;
+}
+</style>
